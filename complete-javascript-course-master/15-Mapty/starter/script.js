0666ec84373a65a0df7be057d92cc7d5fa657173
Syed Eebad Reza <95500079+eebadreza@users.noreply.github.com>
2023-08-10 00:33:11 +0530

    Started Mapty

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -11,23 +11,65 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(function (position){
-    const {latitude} = position.coords;
-    const { longitude } = position.coords;
-    console.log(latitude, longitude);
+// navigator.geolocation.getCurrentPosition(function (position){
+//     const {latitude} = position.coords;
+//     const { longitude } = position.coords;
+//     console.log(latitude, longitude);
 
-    const coords = [latitude, longitude];
-    var map = L.map('map').setView(coords, 13);
+//     const coords = [latitude, longitude];
+//     var map = L.map('map').setView(coords, 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+//     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       attribution:
+//         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
-}, function(){
-    alert("Could not get your location");
-})
+//     L.marker(coords)
+//       .addTo(map)
+//       .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//       .openPopup();
+// }, function(){
+//     alert("Could not get your location");
+// })
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      // console.log(pos);
+      const { latitude } = pos.coords;
+      const { longitude } = pos.coords;
+      // console.log(latitude, longitude);
+
+      const coords = [ latitude, longitude];
+
+      var map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function(mapEvent){
+        
+        // console.log(mapEvent);
+        const {lat, lng} = mapEvent.latlng;
+
+        // console.log(lat, lng);
+
+        L.marker( [lat, lng])
+          .addTo(map)
+          .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+          }))
+          .setPopupContent('Workout')
+          .openPopup();
+      })
+
+    },
+    function () {
+      alert('Could not get your Location');
+    }
+  );
